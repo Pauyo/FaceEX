f78a26bd9258f4486bf2faa30592b823ae0af8fe
Shikanime <deva.shikanime@outlook.com>
2018-03-07 21:40:36 +0100

    :pencil2: parser video ok

--- FaceEx/SelectTargetPage.xaml.cs
+++ FaceEx/SelectTargetPage.xaml.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
-using Windows.Graphics.Imaging;
 using Windows.Media.Editing;
-using Windows.Media.FaceAnalysis;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
-using Windows.UI;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Shapes;
+using Microsoft.ProjectOxford.Face;
 
 namespace FaceEX
 {
@@ -21,14 +18,10 @@ namespace FaceEX
     /// </summary>
     public sealed partial class SelectTargetPage
     {
-        // Style
-        private const double LineThickness = 2.0;
-        private readonly SolidColorBrush _fillBrush = new SolidColorBrush(Colors.Transparent);
-        private readonly SolidColorBrush _lineBrush = new SolidColorBrush(Colors.Yellow);
-
-        // Photo Processor
-        private IList<DetectedFace> _detectedFaces;
-        private FaceDetector _faceDetector;
+        private static readonly IFaceServiceClient FaceServiceClient = new FaceServiceClient(
+            "3d7d23e210144e1ab01e5f7a335d0a1d",
+            "https://westcentralus.api.cognitive.microsoft.com/face/v1.0"
+        );
 
         public SelectTargetPage()
         {
@@ -58,104 +51,64 @@ namespace FaceEX
 
             if (fileData.ContentType.StartsWith("image"))
             {
-                DetectFace_Image(fileData);
+                DetectFace_Image(await fileData.OpenStreamForReadAsync());
             }
             else if (fileData.ContentType.StartsWith("video"))
             {
-                var frame = await GetFrameAsync(fileData);
+                DetectFace_Video(fileData);
+            }
+        }
+
+        private static async void DetectFace_Video(StorageFile fileData)
+        {
+            const int frameJumpSize = 15;
+            var videoProperties = await fileData.Properties.GetVideoPropertiesAsync();
+            for (
+                var time = TimeSpan.FromSeconds(0);
+                time < videoProperties.Duration;
+                time += TimeSpan.FromSeconds(frameJumpSize)
+            ) {
+                var mediaClip = await MediaClip.CreateFromFileAsync(fileData);
+                var mediaComposition = new MediaComposition();
+                mediaComposition.Clips.Add(mediaClip);
+                var frame = await mediaComposition.GetThumbnailAsync(
+                    time,
+                    0,
+                    0,
+                    VideoFramePrecision.NearestFrame
+                );
                 var bitmapImage = new BitmapImage();
                 var randomAccessStream = new InMemoryRandomAccessStream();
                 await RandomAccessStream.CopyAsync(frame, randomAccessStream);
                 randomAccessStream.Seek(0);
                 bitmapImage.SetSource(randomAccessStream);
+
+//                DetectFace_Image(bitmapImage);
+
+                await Task.Delay(20_000);
             }
         }
 
-        public async Task<IInputStream> GetFrameAsync(StorageFile file)
+        private static async void DetectFace_Image(Stream fileData)
         {
-            var mediaClip = await MediaClip.CreateFromFileAsync(file);
-            var mediaComposition = new MediaComposition();
-            mediaComposition.Clips.Add(mediaClip);
-            return await mediaComposition.GetThumbnailAsync(
-                TimeSpan.FromMilliseconds(5000), 0, 0, VideoFramePrecision.NearestFrame);
-        }
+            var faces = await FaceServiceClient.DetectAsync(fileData);
+            var faceIds = faces.Select(face => face.FaceId).ToArray();
 
-        private async void DetectFace_Image(IStorageFile fileData)
-        {
-            // Open file
-            var fileStream = await fileData.OpenAsync(FileAccessMode.Read);
-            var decoder = await BitmapDecoder.CreateAsync(fileStream);
-
-            // Resize to optimize machine learning complexity
-            var transform = new BitmapTransform();
-            const float sourceImageHeightLimit = 1280;
-
-            if (decoder.PixelHeight > sourceImageHeightLimit)
+            var results = await FaceServiceClient.IdentifyAsync("default", faceIds);
+            foreach (var identifyResult in results)
             {
-                var scalingFactor = sourceImageHeightLimit / decoder.PixelHeight;
-                transform.ScaledWidth = (uint) Math.Floor(decoder.PixelWidth * scalingFactor);
-                transform.ScaledHeight = (uint) Math.Floor(decoder.PixelHeight * scalingFactor);
-            }
-
-            // Output transformed file
-            var sourceBitmap = await decoder.GetSoftwareBitmapAsync(decoder.BitmapPixelFormat,
-                BitmapAlphaMode.Premultiplied, transform, ExifOrientationMode.IgnoreExifOrientation,
-                ColorManagementMode.DoNotColorManage);
-            const BitmapPixelFormat faceDetectionPixelFormat = BitmapPixelFormat.Gray8;
-
-            var convertedBitmap = sourceBitmap.BitmapPixelFormat != faceDetectionPixelFormat
-                ? SoftwareBitmap.Convert(sourceBitmap, faceDetectionPixelFormat)
-                : sourceBitmap;
-
-            // Init dectetor
-            if (_faceDetector == null) _faceDetector = await FaceDetector.CreateAsync();
-
-            _detectedFaces = await _faceDetector.DetectFacesAsync(convertedBitmap);
-
-            ShowDetectedFaces(sourceBitmap);
-
-            sourceBitmap.Dispose();
-            fileStream.Dispose();
-            convertedBitmap.Dispose();
-        }
-
-        private async void ShowDetectedFaces(SoftwareBitmap sourceBitmap)
-        {
-            var pixelWidth = sourceBitmap.PixelWidth;
-            var pixelHeight = sourceBitmap.PixelHeight;
-
-            // Treat bitmap image to software bitmap image
-            var brush = new ImageBrush();
-            var bitmapSource = new SoftwareBitmapSource();
-            await bitmapSource.SetBitmapAsync(sourceBitmap);
-            brush.ImageSource = bitmapSource;
-            brush.Stretch = Stretch.Fill;
-
-            // Define Canvas of image
-            VisualizationCanvas.Background = brush;
-
-            if (_detectedFaces == null) return;
-
-            // Fix rectangle scale 
-            var widthScale = pixelWidth / VisualizationCanvas.ActualWidth;
-            var heightScale = pixelHeight / VisualizationCanvas.ActualHeight;
-
-            foreach (var face in _detectedFaces)
-            {
-                var box = new Rectangle
+                if (identifyResult.Candidates.Length == 0)
                 {
-                    Tag = face.FaceBox,
-                    Width = (int) (face.FaceBox.Width / widthScale),
-                    Height = (int) (face.FaceBox.Height / heightScale),
-                    Fill = _fillBrush,
-                    Stroke = _lineBrush,
-                    StrokeThickness = LineThickness,
-                    Margin = new Thickness((uint) (face.FaceBox.X / widthScale), (uint) (face.FaceBox.Y / heightScale),
-                        0, 0)
-                };
-
-                VisualizationCanvas.Children.Add(box);
+                    //TODO no match handler
+                }
+                else
+                {
+                    var candidateId = identifyResult.Candidates[0].PersonId;
+                    var person = await FaceServiceClient.GetPersonInLargePersonGroupAsync("default", candidateId);
+                    //TODO success handler
+                }
             }
+
         }
     }
 }
